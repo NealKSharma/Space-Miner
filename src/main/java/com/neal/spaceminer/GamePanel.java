b6bf479cded21a7552ace095c49ea1533899b4f1
NealKSharma <sharneal11@gmail.com>
2025-10-30 23:04:32 -0500

    v0.1.1 - Added a movable rectangle controlled by key inputs which updates at 144 FPS

--- src/main/java/com/neal/spaceminer/GamePanel.java
+++ src/main/java/com/neal/spaceminer/GamePanel.java
@@ -15,12 +15,22 @@ public class GamePanel extends JPanel implements Runnable {
     final int screenWidth = tileSize * maxScreenCol; // 1024px (64x16)
     final int screenHeight = tileSize * maxScreenRow; // 768px (64x12)
 
+    int FPS = 144;
+
+    KeyHandler keyHandler = new KeyHandler();
     Thread gameThread;
+
+    // Player's default position
+    int playerX = 100;
+    int playerY = 100;
+    int speed = 4;
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyHandler);
+        this.setFocusable(true);
     }
 
     public void startGame() {
@@ -31,5 +41,55 @@ public class GamePanel extends JPanel implements Runnable {
     @Override
     public void run() {
 
+        double drawInterval = 1000000000.0 / FPS;
+        double nextDrawTime = System.nanoTime() + drawInterval;
+
+        while (gameThread.isAlive()) {
+
+            update();
+            repaint();
+
+            try {
+                double remainingTime = nextDrawTime - System.nanoTime();
+                remainingTime /= 1000000;
+
+                if(remainingTime < 0){
+                    remainingTime = 0;
+                }
+
+                Thread.sleep((long)remainingTime);
+
+                nextDrawTime += drawInterval;
+            }
+            catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public void update(){
+        if (keyHandler.up){
+            playerY -= speed;
+        }
+        if (keyHandler.down){
+            playerY += speed;
+        }
+        if (keyHandler.left){
+            playerX -= speed;
+        }
+        if (keyHandler.right){
+            playerX += speed;
+        }
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D) g;
+
+        g2.setColor(Color.white);
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
+
+        g2.dispose();
     }
 }
